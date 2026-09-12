e41a85d36a4fe9a29e6eb76f596e169165492e53
marufabsm26-source <marufabsm26@gmail.com>
2026-09-13 03:06:50 +0600

    Typwr

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
+import { Suspense, useState } from 'react'
 import Nav from './Components/Nav'
 import Bannar from './Components/Bannar'
+import Technology from './Components/Technology'
+import type { Tprops } from './type'
 
+const Technologyfetch = async (): Promise<Tprops[]> => {
+const res = await fetch('/public/data.json')
+const data = await res.json()
+return data 
+}
 function App() {
-  const [count, setCount] = useState(0)
-
+const Technologys = Technologyfetch()
   return (
     <>
      <Nav />
      <Bannar />
+     <Suspense fallback={<div>Loading...</div>}>
+     <Technology Technologies={Technologys} />
+     </Suspense>
     </>
   )
 }
---
+++ src/Components/Technology.tsx
@@ -0,0 +1,13 @@
+import React, { use } from 'react';
+import type { Tprops } from '../type';
+
+const Technology = ({ Technologies }: { Technologies: Tprops[] }) => {
+  const TechnologyData = use(Technologies);
+  return (
+    <div> 
+      
+    </div>
+  );
+};
+
+export default Technology;
